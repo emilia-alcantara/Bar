ad14631b07fbcca4680f57d56368ed7cbabd306d
Emilia <correoalcantaraemilia@gmail.com>
2023-07-28 21:15:23 -0400

    Creación del repositorio.

---
+++ app/src/main/java/cl/individual/viernes280723/data/Repositorio.kt
@@ -0,0 +1,17 @@
+package cl.individual.viernes280723.data
+
+import androidx.lifecycle.LiveData
+import cl.individual.viernes280723.data.local.BarDao
+import cl.individual.viernes280723.data.local.Item
+
+class Repositorio (private val barDao : BarDao) {
+
+    suspend fun insertItem(item: Item){
+        barDao.insertItem(item)
+    }
+
+    fun getItemList(): LiveData<List<Item>> {
+        return barDao.getItemList()
+    }
+
+}
